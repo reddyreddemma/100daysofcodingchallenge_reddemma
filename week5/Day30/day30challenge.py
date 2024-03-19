fdd8d86d15203c62db41d5cfbc0612fd74e8fb71
reddemma <148742301+reddyreddemma@users.noreply.github.com>
2024-03-19 19:15:42 +0530

    Create day30challenge.py

---
+++ week5/Day30/day30challenge.py
@@ -0,0 +1,17 @@
+# Declare variables
+age = 19
+height = 5.30
+first_name = "reddemma"
+
+# Concatenate first name with a greeting message and print the result
+greeting_message = "Hi ,it's " + first_name + "!"
+print(greeting_message)
+
+# Calculate the square of age and print the result
+age_square = age ** 2
+print("The square of your age is:", age_square)
+
+# Calculate BMI and print the result
+weight = 50
+bmi = weight / (height ** 2)
+print("Your BMI is:", bmi)
